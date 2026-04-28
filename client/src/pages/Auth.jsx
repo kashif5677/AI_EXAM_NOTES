5f5a71af4ca9a7c7f30bf84299dae7a9c8b385ba
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-28 23:55:44 +0530

    shadow

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -9,7 +9,7 @@ function Auth() {
         className='max-w-7xl mx-auto mt-8
             rounded-2xl bg-black/80 backdrop-blur-xl 
              px-8 py-4
-            // shadow-[0_20px_45px_rgba(0,0,0,0.6)'
+             shadow-[0_20px_45px_rgba(0,0,0,0.6)'
             id="header"
       >
 
