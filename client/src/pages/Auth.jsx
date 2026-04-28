37e2060e7ae22afa30a4bc5e62cba07a08c18c4c
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-28 23:55:12 +0530

    line edit

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+function Auth() {
+  return (
+    <div className='min-h-screen overflow-hidden bg-white text-black px-8'>
+      <motion.header 
+      initial=
+        className='max-w-7xl mx-auto mt-8
+            rounded-2xl bg-black/80 backdrop-blur-xl 
+             px-8 py-4
+            // shadow-[0_20px_45px_rgba(0,0,0,0.6)'
+            id="header"
+      >
+
+      </motion.header>
+      
+  
+    </div>
+  )
+}
+
+export default Auth
