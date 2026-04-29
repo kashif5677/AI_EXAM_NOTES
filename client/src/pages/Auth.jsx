737824ee7be734b3fce720fec86846ee14a065c7
mdkashif <mdkashifnisar5677@gmail.com>
2026-04-29 23:54:36 +0530

    framer motion

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -8,7 +8,7 @@ function Auth() {
       initial={{opacity:0,y:-15}}
       animate={{opacity:1,y:0}}
       transition={{duration:0.5}}
-        className='max-w-7xl mx-auto mt-8
+        // className='max-w-7xl mx-auto mt-8
             rounded-2xl bg-black/80 backdrop-blur-xl 
              px-8 py-4
              shadow-[0_20px_45px_rgba(0,0,0,0.6)'
